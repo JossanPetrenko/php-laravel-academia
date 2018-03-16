9212f41016105ebdcb095d353b9c65f734708e9f
Unknown <jossanpetrenko@hotmail.com>
2018-03-16 14:08:51 -0400

    Login Views

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -14,3 +14,12 @@
 Route::get('/', function () {
     return view('home');
 });
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
